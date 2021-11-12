c5631109c871dab429247bfab59825d310dbfddd
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-11-12 17:10:12 +0200

    add updateContact.js

---
+++ controllers/contacts/updateContact.js
@@ -0,0 +1,24 @@
+const fs = require("fs/promises");
+const contacts = require("../../model/contacts.json");
+const path = require("path");
+
+const contactsPath = path.resolve("");
+
+const updateContact = async (contactId, body) => {
+  const changeContact = await contacts.map((element) => {
+    if (element.id === contactId) {
+      element = { ...element, ...body };
+    }
+    return element;
+  });
+  fs.writeFile(
+    `${contactsPath}/model/contacts.json`,
+    JSON.stringify(changeContact),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  return changeContact;
+};
+
+module.exports = updateContact;
