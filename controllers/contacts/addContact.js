fc032623a6b3087876362ce5b00120310b31f680
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-11-12 17:08:58 +0200

    add addContact.js

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,21 @@
+const fs = require("fs/promises");
+const contacts = require("../../model/contacts.json");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.resolve("");
+
+const addContact = async (body) => {
+  const currentContact = { id: uuidv4(), ...body };
+  const contact = [...contacts, currentContact];
+  fs.writeFile(
+    `${contactsPath}/model/contacts.json`,
+    JSON.stringify(contact),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  return contact;
+};
+
+module.exports = addContact;
