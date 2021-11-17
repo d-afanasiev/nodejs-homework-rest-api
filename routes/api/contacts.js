921a7e4d2566b4ae4320ff06d47d9d0c0c366282
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-11-17 10:15:35 +0200

    изменил router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,56 +6,21 @@ const {
 } = require("../../middlewares/validationMidleware");
 
 const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
+  getListContacts,
+  getContact,
+  addContactToFile,
+  deleteContact,
+  renewContact,
 } = require("../../controllers/contacts");
 
-router.get("/", async (req, res, next) => {
-  const contacts = await listContacts();
-  return res.status(200).json(contacts);
-});
+router.get("/", getListContacts);
 
-router.get("/:contactId", async (req, res, next) => {
-  const id = parseInt(req.params.contactId);
+router.get("/:contactId", getContact);
 
-  const contact = await getContactById(id);
-  if (!contact) {
-    return res.status(404).json({ message: "Not found" });
-  }
-  return res.status(200).json(contact);
-});
+router.post("/", addContactValidation, addContactToFile);
 
-router.post("/", addContactValidation, async (req, res, next) => {
-  const writeContact = await addContact(req.body);
-  return res.status(201).json(writeContact);
-});
+router.delete("/:contactId", deleteContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  const id = parseInt(req.params.contactId);
-  const deleteContact = await removeContact(id);
-  if (!deleteContact) {
-    return res.status(404).json({ message: "Not found" });
-  }
-  return res.status(200).json({ message: "contact deleted" });
-});
-
-router.put("/:contactId", patchContactValidation, async (req, res, next) => {
-  const id = parseInt(req.params.contactId);
-  const bodyContact = req.body;
-
-  if (Object.keys(bodyContact).length === 0) {
-    return res.status(400).json({ message: "missing fields" });
-  }
-  const newContact = await updateContact(id, bodyContact);
-
-  if (!newContact) {
-    return res.status(404).json({ message: "Not found" });
-  }
-
-  return res.status(200).json(newContact);
-});
+router.put("/:contactId", patchContactValidation, renewContact);
 
 module.exports = router;
