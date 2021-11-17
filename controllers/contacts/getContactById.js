bacd4cfa202bbdb0aa13c40b62cadadbdcd8e952
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-11-17 10:12:31 +0200

    перенес файлы в другую папку

--- controllers/contacts/getContactById.js
+++
@@ -1,8 +0,0 @@
-const contacts = require("../../model/contacts.json");
-
-const getContactById = async (contactId) => {
-  const searchContact = contacts.find(({ id }) => id === contactId);
-  return searchContact;
-};
-
-module.exports = getContactById;
--- controllers/contacts/listContacts.js
+++
@@ -1,8 +0,0 @@
-const contacts = require("../../model/contacts.json");
-
-const listContacts = async () => {
-  const allContacts = await contacts;
-  return allContacts;
-};
-
-module.exports = listContacts;
