3ae01ad6296625aec701b37ed1f33aace1226229
Afanasiev Dmitriy <afanasiev.dimon@outlook.com>
2021-11-18 21:54:52 +0200

    add favorite to midleware

--- middlewares/validationMidleware.js
+++ middlewares/validationMidleware.js
@@ -14,7 +14,7 @@ module.exports = {
         .length(10)
         .pattern(/^[0-9]+$/)
         .required(),
-      favorite: Joi.boolean().required(),
+      favorite: Joi.boolean(),
     });
 
     const validationResult = await schema.validate(req.body);
@@ -26,7 +26,7 @@ module.exports = {
     next();
   },
 
-  patchContactValidation: async (req, res, next) => {
+  putContactValidation: async (req, res, next) => {
     const schema = await Joi.object({
       name: Joi.string().alphanum().min(3).max(30),
       email: Joi.string().email({
@@ -37,6 +37,19 @@ module.exports = {
         .length(10)
         .pattern(/^[0-9]+$/),
       favorite: Joi.boolean(),
+    });
+
+    const validationResult = await schema.validate(req.body);
+
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error.details });
+    }
+
+    next();
+  },
+  patchContactValidation: async (req, res, next) => {
+    const schema = await Joi.object({
+      favorite: Joi.boolean().required(),
     });
 
     const validationResult = await schema.validate(req.body);
