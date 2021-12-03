8eb1538a914786662e5b6a001f6f98a842ff8615
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-12-03 15:24:36 +0200

    add gravatar

--- db/authModel.js
+++ db/authModel.js
@@ -16,6 +16,7 @@ const authSchema = new Schema({
     enum: ["starter", "pro", "business"],
     default: "starter",
   },
+  avatarURL: String,
   token: {
     type: String,
     default: null,
--- models/auth/registration.js
+++ models/auth/registration.js
@@ -1,8 +1,10 @@
 const { User } = require("../../db/authModel");
+const gravatar = require("gravatar");
 
 const registration = async (body) => {
   const { password, email, subscription, token } = body;
-  const newUser = new User({ email, subscription, token });
+  const avatarURL = gravatar.url(email, { s: "100", r: "x", d: "retro" }, true);
+  const newUser = new User({ email, subscription, token, avatarURL });
   newUser.setPassword(password);
   await newUser.save();
   return newUser;
